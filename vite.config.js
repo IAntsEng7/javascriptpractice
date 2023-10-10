96a1e32c011093a6c8726aebfdc0e4f763046561
Caerus <snake7726@gmail.com>
2023-10-10 16:26:26 +0800

    Tool Install: ESLint

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import eslintPlugin from "vite-plugin-eslint" // ** add this for ESLint: auto format when save **
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import eslintPlugin from "vite-plugin-eslint"; // ** add this for ESLint: auto format when save **
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    eslintPlugin({ cache: false }) // ** add this for ESLint: auto format when save **
+    eslintPlugin({ cache: false }), // ** add this for ESLint: auto format when save **
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
