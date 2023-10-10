8460c4836db69d2892e470f7f8cbd4ab4fe32f51
Caerus <snake7726@gmail.com>
2023-10-10 19:44:11 +0800

    Tool Install: i18n

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,47 +1,24 @@
-<script setup>
-defineProps({
-  msg: {
-    type: String,
-    required: true,
-  },
-});
-</script>
-
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
-    <h1 class="text-3xl font-bold underline">
-      Hello!! TailwindCSS Install Success!!
-    </h1>
+  <div class="card">
+    <div>
+      切換語言：
+      <select @change="handleChangeLanguage">
+        <option value="zh-TW" selected>中文</option>
+        <option value="en-US">English</option>
+      </select>
+    </div>
+    <p>
+      {{ $t("edit") }} <code>components/HelloWorld.vue</code>
+      {{ $t("test_i18n_func") }}
+    </p>
   </div>
 </template>
 
-<style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
+<script setup lang="ts">
+import { useI18n } from "vue-i18n";
 
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-}
-</style>
+const { locale } = useI18n();
+const handleChangeLanguage = (e: Event) => {
+  locale.value = (e.target as HTMLSelectElement).value;
+};
+</script>
